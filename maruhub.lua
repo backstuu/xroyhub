7d6ca0eb0dc843b311c195a5dd22b9e0b1985caf
backstuu <59038651+backstuu@users.noreply.github.com>
2023-05-25 20:13:26 +0700

    Update maruhub.lua

--- maruhub.lua
+++ maruhub.lua
@@ -1,59 +1,57 @@
 
 
 
+
+
 getgenv().Key = "MARU-ZCM4E-RUO7K-ATSW-ZCRXD-2SGC"
 getgenv().id = "243198428367028224"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
-
-_G.Settings.AutoFarmLevel = "true" -- true / false Auto Farm Level
-_G.Settings.AutoSecondSea = "true" -- true / false Auto Second Sea
-_G.Settings.AutoFarmFactory = "false" -- true / false Auto Farm Factory
-_G.Settings.AutoThirdSea = "true" -- true / false Auto Third Sea
-_G.Settings.AutoPiratesRaid = "false" -- true / false Auto Pirates Raid
-_G.Settings.AutoFarmFruitMastery = "false" -- true / false Auto Farm Fruit Mastery
-_G.Settings.AutoFarmGunMastery = "false" -- true / false Auto Farm Gun Mastery
+_G.Settings.AutoFarmLevel = true -- true / false Auto Farm Level
+_G.Settings.AutoSecondSea = true -- true / false Auto Second Sea
+_G.Settings.AutoFarmFactory = false -- true / false Auto Farm Factory
+_G.Settings.AutoThirdSea = true -- true / false Auto Third Sea
+_G.Settings.AutoPiratesRaid = false -- true / false Auto Pirates Raid
+_G.Settings.AutoFarmFruitMastery = false -- true / false Auto Farm Fruit Mastery
+_G.Settings.AutoFarmGunMastery = false -- true / false Auto Farm Gun Mastery
 _G.Settings.SelectWeapon = ""  -- Your Weapons / Combat
-_G.Settings.FastAttack = "true" -- true / false Fast Attack
-_G.Settings.SuperFastAttack = "true" -- true / false Super Fast Attack
-_G.Settings.SuperFastAttack2 = "true" -- true / false Super Fast Attack V2
-_G.Settings.Clip = "false" -- true / false -- Clip
-_G.Settings.Disabled_3dRendering = "false" -- true / false Black Screen
-_G.Settings.SkillZMas = "true" -- true / false Skill Z Mas
-_G.Settings.SkillXMas = "true" -- true / false Skill X Mas
-_G.Settings.SkillCMas = "true" -- true / false Skill C Mas
-_G.Settings.SkillVMas = "true" -- true / false Skill V Mas
-_G.Settings.AutoDeathStep = "true" -- true / false Auto Death Step
+_G.Settings.FastAttack = true -- true / false Fast Attack
+_G.Settings.SuperFastAttack = true -- true / false Super Fast Attack
+_G.Settings.SuperFastAttack2 = true -- true / false Super Fast Attack V2
+_G.Settings.Clip = false -- true / false -- Clip
+_G.Settings.Disabled_3dRendering = false -- true / false Black Screen
+_G.Settings.SkillZMas = true -- true / false Skill Z Mas
+_G.Settings.SkillXMas = true -- true / false Skill X Mas
+_G.Settings.SkillCMas = true -- true / false Skill C Mas
+_G.Settings.SkillVMas = true -- true / false Skill V Mas
+_G.Settings.AutoDeathStep = true -- true / false Auto Death Step
 _G.Settings.SelectFruit = {"Bomb-Bomb","Spike-Spike","Chop-Chop","Spring-Spring","Kilo-Kilo","Spin-Spin","Bird: Falcon","Smoke-Smoke","Flame-Flame","Ice-Ice","Sand-Sand","Dark-Dark","Revive-Revive","Diamond-Diamond","Light-Light","Love-Love","Rubber-Rubber","Barrier-Barrier","Magma-Magma","Door-Door","Quake-Quake","Human-Human: Buddha","String-String", "Bird-Bird: Phoenix","Rumble-Rumble","Paw-Paw","Gravity-Gravity","Dough-Dough","Venom-Venom","Shadow-Shadow","Control-Control","Soul-Soul","Dragon-Dragon","Leopard-Leopard"}
-_G.Settings.AutoBuyFruitSniper = "false" -- true / false Auto Buy FruitSniper
+_G.Settings.AutoBuyFruitSniper = false -- true / false Auto Buy FruitSniper
 _G.Settings.SelectFruitEat = {"Bomb-Bomb","Spike-Spike","Chop-Chop","Spring-Spring","Kilo-Kilo","Spin-Spin","Bird: Falcon","Smoke-Smoke","Flame-Flame","Ice-Ice","Sand-Sand","Dark-Dark","Revive-Revive","Diamond-Diamond","Light-Light","Love-Love","Rubber-Rubber","Barrier-Barrier","Magma-Magma","Door-Door","Quake-Quake","Human-Human: Buddha","String-String", "Bird-Bird: Phoenix","Rumble-Rumble","Paw-Paw","Gravity-Gravity","Dough-Dough","Venom-Venom","Shadow-Shadow","Control-Control","Soul-Soul","Dragon-Dragon","Leopard-Leopard"}
-_G.Settings.Random_Auto = "true" -- true / false Random Auto 
-_G.Settings.AutoStoreFruit = "true" -- true / false Auto Store Fruit
-_G.Settings.BringFruit = "false" -- true / false Bring Fruit
-_G.Settings.EnabledFake = "false" -- true / false Enabled Fake
-_G.Settings.MobAuraFarmMatery = "false" -- true / false Mob Aura FarmMatery
-_G.Settings.MobAuraFarmMateryGun = "false" -- true / false Mob Aura FarmMateryGun
-_G.Settings.BossLaw = "false" -- true / false Boss Law
-_G.Settings.BuyBossLawChip = "false" -- true / false Buy Boss LawChip
-_G.Settings.ESPChests = "false" -- true / false ESP Chests
-_G.Settings.ESPMystic = "false" -- true / false ESP Mystic
-_G.Settings.AutoFarmAllBosses = "false" -- true / false Auto Farm AllBosses
-_G.Settings.AutoMaxMaterySword = "false" -- true / false Auto Max MaterySword
-_G.Settings.EnabledHopCDk = "false" -- true / false Enabled HopCDk
-_G.Settings.NClip = "false" -- true / false N Clip
-_G.Settings.INFOBRange = "false" -- true / false INFOB Range
+_G.Settings.Random_Auto = true -- true / false Random Auto 
+_G.Settings.AutoStoreFruit = true -- true / false Auto Store Fruit
+_G.Settings.BringFruit = false -- true / false Bring Fruit
+_G.Settings.EnabledFake = false -- true / false Enabled Fake
+_G.Settings.MobAuraFarmMatery = false -- true / false Mob Aura FarmMatery
+_G.Settings.MobAuraFarmMateryGun = false -- true / false Mob Aura FarmMateryGun
+_G.Settings.BossLaw = false -- true / false Boss Law
+_G.Settings.BuyBossLawChip = false -- true / false Buy Boss LawChip
+_G.Settings.ESPChests = false -- true / false ESP Chests
+_G.Settings.ESPMystic = false -- true / false ESP Mystic
+_G.Settings.AutoFarmAllBosses = false -- true / false Auto Farm AllBosses
+_G.Settings.AutoMaxMaterySword = false -- true / false Auto Max MaterySword
+_G.Settings.EnabledHopCDk = false -- true / false Enabled HopCDk
+_G.Settings.NClip = false -- true / false N Clip
+_G.Settings.INFOBRange = false -- true / false INFOB Range
 -- Stats
 _G.Settings.PointsUp = 1 -- true / false
-_G.Settings.AutoMelee = "true" -- true / false Auto Melee
-_G.Settings.AutoDefense = "true" -- true / false Auto Defense
-_G.Settings.AutoSword = "false" -- true / false Auto Sword
-_G.Settings.AutoGun = "false" -- true / false Auto Gun
-_G.Settings.AutoDevilFruits = "false"
-_G.Settings.RTXMode = "true"
-_G.Settings.RemoveFogs = "true"
-_G.Settings.INFEnergy = "false"
-_G.Settings.AutoAgility = "false"
-_G.Settings.InfAb = "false"
-_G.Settings.EnabledFakeStat = "false"
-
-
-
+_G.Settings.AutoMelee = true -- true / false Auto Melee
+_G.Settings.AutoDefense = true -- true / false Auto Defense
+_G.Settings.AutoSword = false -- true / false Auto Sword
+_G.Settings.AutoGun = false -- true / false Auto Gun
+_G.Settings.AutoDevilFruits = false
+_G.Settings.RTXMode = true
+_G.Settings.RemoveFogs = true
+_G.Settings.INFEnergy = false
+_G.Settings.AutoAgility = false
+_G.Settings.InfAb = false
+_G.Settings.EnabledFakeStat = false
