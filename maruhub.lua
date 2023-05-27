89a83a2d68e7ad8b6f31eab238a59a3fbc22c94e
backstuu <59038651+backstuu@users.noreply.github.com>
2023-05-27 20:40:00 +0700

    Update maruhub.lua

--- maruhub.lua
+++ maruhub.lua
@@ -1,9 +1,27 @@
 
 
 
+
+_G.Settings = {
+    ['AutoFarmLevel'] = true,
+    ['AutoSecondSea'] = true, -- true / false -- จอขาว
+    ['AutoThirdSea'] = true,
+    ['FastAttack'] = true,
+    ['SuperFastAttack'] = true,
+    ['SuperFastAttack2'] = true,
+    ['AutoStoreFruit'] = true,
+    ['AutoDeathStep'] = true,
+    ['Random_Auto'] = true,
+    ['BringFruit'] = true,
+    ['PointsUp'] = 1,
+    ['AutoMelee'] = true,
+    ['AutoDefense'] = true,
+}
+
+
+
+
 getgenv().Key = "MARU-ZCM4E-RUO7K-ATSW-ZCRXD-2SGC"
 getgenv().id = "243198428367028224"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
-_G.Settings.AutoFarmLevel = "true"
-_G.Settings.AutoSecondSea = "true"
-_G.Settings.AutoThirdSea = "true"
+
