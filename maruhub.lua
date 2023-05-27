afd5a2f79f1be46ca4b0dfe7bd582511e43facc1
backstuu <59038651+backstuu@users.noreply.github.com>
2023-05-27 19:17:26 +0700

    Update maruhub.lua

--- maruhub.lua
+++ maruhub.lua
@@ -1,10 +1,10 @@
 [  Blox Fruits New Configs (Mobile Only!)]
 true = เปิด, false = ปิด
 -- Settings [ Main ]
-_G.Settings.AutoFarmLevel = false -- true / false Auto Farm Level
-_G.Settings.AutoSecondSea = false -- true / false Auto Second Sea
-_G.Settings.AutoFarmFactory = false -- true / false Auto Farm Factory
-_G.Settings.AutoThirdSea = false -- true / false Auto Third Sea
+_G.Settings.AutoFarmLevel = true -- true / false Auto Farm Level
+_G.Settings.AutoSecondSea = true -- true / false Auto Second Sea
+_G.Settings.AutoFarmFactory = true -- true / false Auto Farm Factory
+_G.Settings.AutoThirdSea = true -- true / false Auto Third Sea
 _G.Settings.AutoPiratesRaid = false -- true / false Auto Pirates Raid
 _G.Settings.AutoFarmFruitMastery = false -- true / false Auto Farm Fruit Mastery
 _G.Settings.AutoFarmGunMastery = false -- true / false Auto Farm Gun Mastery
