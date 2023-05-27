c97fb9d2ac492a74418257ea5febb8fccb72084a
backstuu <59038651+backstuu@users.noreply.github.com>
2023-05-27 19:28:41 +0700

    Update maruhub.lua

--- maruhub.lua
+++ maruhub.lua
@@ -1,4 +1,9 @@
-
+-- Settings [ Main ]
+_G.Settings.AutoFarmLevel = true -- true / false Auto Farm Level
+_G.Settings.AutoSecondSea = true -- true / false Auto Second Sea
+_G.Settings.AutoFarmFactory = true -- true / false Auto Farm Factory
+_G.Settings.AutoThirdSea = true -- true / false Auto Third Sea
+_G.Settings.AutoPiratesRaid = true -- true / false Auto Pirates Raid
 
 getgenv().Key = "MARU-ZCM4E-RUO7K-ATSW-ZCRXD-2SGC"
 getgenv().id = "243198428367028224"
