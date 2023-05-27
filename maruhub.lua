8d90b3c23d4fad7bfb22fbfbf99f0939d0121873
backstuu <59038651+backstuu@users.noreply.github.com>
2023-05-27 20:11:47 +0700

    Update maruhub.lua

--- maruhub.lua
+++ maruhub.lua
@@ -4,6 +4,6 @@
 getgenv().Key = "MARU-ZCM4E-RUO7K-ATSW-ZCRXD-2SGC"
 getgenv().id = "243198428367028224"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
-_G.Settings.AutoFarmLevel = true
-_G.Settings.AutoSecondSea = true
-_G.Settings.AutoThirdSea = true
+_G.Settings.AutoFarmLevel = "true"
+_G.Settings.AutoSecondSea = "true"
+_G.Settings.AutoThirdSea = "true"
