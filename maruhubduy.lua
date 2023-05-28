f4a6f58b3acef0df75ab2d451e19401d5af5aba4
backstuu <59038651+backstuu@users.noreply.github.com>
2023-05-28 14:38:15 +0700

    Update and rename maruhub.lua to maruhubduy.lua

---
+++ maruhubduy.lua
@@ -0,0 +1,3 @@
+getgenv().Key = "MARU-OIM25-PCBLC-E395-5PGUT-WOH0"
+getgenv().id = "1108386152433586196"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
