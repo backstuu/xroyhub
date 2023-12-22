d82d2e20d9fb4c76950aa853f9b1c403e4ed875b
backstuu <59038651+backstuu@users.noreply.github.com>
2023-12-22 17:04:55 +0700

    Update kaitun_sangpc.lua

--- kaitun_sangpc.lua
+++ kaitun_sangpc.lua
@@ -1,94 +1,41 @@
-_G.KaitunConfig = {
-	["Auto Farm Level"] = true,
-	["Auto Boost FPS"] = true,
-	["Skip Level Farm"] = false,
-	["Fast Attack Mode"] = "Fast Attack", -- "Fast Attack" , "More Then old Fast Attack" 
-	["Hop If Can't Kill Player"] = true,
-	["Block All Hop"] = false,
-	
-	["Start Kaitun"] = true,
-	-- World 1
-	["Auto Pole"] = true, -- จะตีเเค่ถ้ามันเกิดไม่ได้ตีจนกว่าจะได้
-	["Auto Saber"] = true,
-	["Auto Second World"] = true,
-	-- World 2
-	["Auto Rengoku"] = false,
-	["Auto Quest Flower"] = true,
-	["Auto V3 Race"] = false,
-	["Auto Bartilo"] = false,
-	["Auto Cursed Captain"] = true,
-	["Auto Darkbeard"] = true,
-	["Auto Factory"] = true,
-	["Auto Third World"] = true,
-	["Skip Get Item Guitar"] = true, -- จะไม่ หาของทำ soul guiter ในโลก 2 เบบ หาจนกว่าจะได้ will not find item until get all item for do soul guiter ( open recommend เเนะนำให้เปิด )
-	["Allise Fruit"] = {"Dragon-Dragon","Spirit-Spirit","Venom-Venom","Dough-Dough"}, -- จะไม่ใช้ผลพวกนี้ในการเปิดประตูไปโลก3
-	-- World 3
-	["Auto Hallow Scythe"] = false,
-	["Auto Buddy Sword"] = false,
-	["Auto Dough King"] = false,
-	["Auto Spikey Trident"] = false,
-	["Auto Tushita"] = false,
-	["Auto Elite Hunter"] = false,
-	["Auto rip Indra"] = false,
-	["Auto Yama"] = false,
-	["Auto Canvander"] = false,
-	["Auto Soul Guitar"] = true, 
-	["Auto Rainbow Haki"] = false,
-	["Auto Cursed Dual Katana"] = false,
-	
-	-- Fighting Style 
-	
-	["Auto God Human"] = true,
-	["Auto Superhuman"] = true,
-	["Auto Death Step"] = true,
-	["Auto Sharkman Karate"] = true,
-	["Auto Electric Claw"] = true,
-	["Auto Dargon Talon"] = true,
-	
-	["Auto Mastery Devil Fruit"] = true,
-	["Skill Setting"] = { -- ถ้าไม่ใส่จะใช้ mode auto
-		-- ["Z"] = 0.1,
-		-- ["X"] = 0.1,
-		-- ["C"] = 0.1,
-		-- ["V"] = 0.1, -- อันไหนไม่เอาลบออกไปเลย
-	},
-	["Auto Mastery Sword"] = false,
-	["Sword Rarity Select"] = {"Mythical","Legendary"}, -- Common , Uncommon,Rare,Legendary,Mythical
 
-	["Select Redeem Code Level"] = 1,
-
-	-- Raids
-
-	["Select Rate Fruit Rais"] = 1000000, -- if fruit price less u rate then we use it to auto raids
-	["Limit Raids Fragments"] = 100000,
-
-	-- Devil Fruit
-
-	["Select Main Devil Fruit Sniper"] = {"Dragon-Dragon","Spirit-Spirit","Venom-Venom","Dough-Dough"}, -- ผลหลักที่จะกินเเทนผลรอง
-	["Select Sub Devil Fruit Sinper"] = {"Ice-Ice","Sand-Sand","Dark-Dark","Quake-Quake","Light-Light"}, -- ผลรองจะกินไว้ก่อนเเล้วพอผลหลักมีก้จะเปลียนไปกินผิดหลัก
-	["Allow Eat Fruit In Inventory"] = true,
-	["Start Sniper"] = true,
-
-	-- RAM
-
-	["RAM Port"] = 7963,
-	["RAM Password"] = "",
-	["Auto Set Description"] = false,
-
-	-- Webhook
-
-	["Start Webhook"] = false,
-	["Link Webhook"] = "",
-	["Mode Webhook"] = "Send On Level Max", -- "Send Every 10 min","Send On Level Max","Send On Level Max And Every 10 min"
-
-	-- CPU
-	
-	["Select Lock Fps"] = 45,
-	["Lock Fps Now"] = true,
-	["White Screen"] = true
+getgenv().DitBF = {
+    ["Performance"] = {
+        ["Black Screen"] = true,
+        ["Lock FPS"] = 30,
+    },
+    ["Raid if Maxed Blox Fruit"] = true,
+    ["Farm boss drops while not maxed"] = false,
+    ["Farm Blox Fruit Mastery if maxed"] = false,
+    ["Farm method after maxed"] = "Raid Boss Farm - Cake Prince Farm",
+    ["Extra time Farm until unlock skills"] = true,
+    ["Hop Server"] = {
+        ["Type"] = {
+            ["[Main] Server Hop"] = true,
+            ["[Farm] Server Hop if Player nearby"] = false,
+            ["[Sea 3 Quest] Server Hop for 1M+ Blox Fruit"] = true,
+        },
+        ["Delay"] = 60,
+    },
+    ["Do Action"] = {
+        ["Get Godhuman"] = true,
+        ["Get Rengoku"] = false,
+        ["Get True Triple Katana"] = false,
+        ["Get Hallow Scythe"] = false,
+        ["Get Cursed Dual Katana"] = true,
+        ["Get Soul Guitar"] = false,
+        ["Awake Current Blox Fruit"] = false,
+    },
+    ["Buy Haki"] = {
+        ["Enhancement"] = false,
+        ["Skyjump"] = false,
+        ["Flash Step"] = false,
+        ["Observation"] = false,
+    },
+    ["Auto Race"] = "None",
+    ["Blox Fruit Sniper"] = {},
+    ["Main Blox Fruit"] = {},
+    ["Eat Sniper Blox Fruits"] = false,
 }
-
-_G.Key = "EVAUN-X3DJ3-QD383"
-_G.DiscordId = "243198428367028224"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Natsuhanaki/Royx_PC/main/loader.lua"))();
-
+getgenv().Key = "k453de01e672ca5089f12b17"
+loadstring(game:HttpGet("https://ditbloxfruit.cc/client.lua"))()  ```
