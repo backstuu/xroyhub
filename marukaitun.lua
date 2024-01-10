b50d1d329564de4f40fbd273526396242aaef9a6
backstuu <59038651+backstuu@users.noreply.github.com>
2024-01-11 01:11:35 +0700

    Update marukaitun.lua

--- marukaitun.lua
+++ marukaitun.lua
@@ -102,5 +102,6 @@ _G.GunSettings = { -- ปืนที่จะทำ
 
 getgenv().Key = "MARU-G3JB1-KZRQA-YVQO-DO5RE-3JW0"
 getgenv().id = "1108370971850707075"
+getgenv().Script_Mode = "Kaitun_Script"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
 
