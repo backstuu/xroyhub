9f79437aa94e8ba34903054b154a6a8e5ad5a068
backstuu <59038651+backstuu@users.noreply.github.com>
2024-01-11 01:33:12 +0700

    Update marukaitun.lua

--- marukaitun.lua
+++ marukaitun.lua
@@ -99,9 +99,9 @@ _G.GunSettings = { -- ปืนที่จะทำ
     ['SerpentBow'] = true,
     ['SoulGuitar'] = true
 }
+-- Script Here !!!
 
 getgenv().Key = "MARU-G3JB1-KZRQA-YVQO-DO5RE-3JW0"
 getgenv().id = "1108370971850707075"
-getgenv().Script_Mode = "Kaitun_Script"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
 
