87bca423e8f54bcfad22b7f54ca0405ca5768e4d
backstuu <59038651+backstuu@users.noreply.github.com>
2024-01-11 01:23:24 +0700

    Update xroykaitun.lua

--- xroykaitun.lua
+++ xroykaitun.lua
@@ -1,4 +1,19 @@
 _G.JoinTeam = "Pirate" -- "Marine" & "Pirate"
+_G.KaitunConfig = {
+    ["Start Farm"] = true,
+    ["PlayerHunter"] = true,
+    ["Auto Fruit"] = true,
+    ["RedeemCode Level"] = 2,
+    
+    
+    ["Black Sreen"] = true,
+    -- sea 2
+    ["Auto Factory"] = true,
+    ["Sea 3 Hop"] = false, -- hop to find fruit
+    
+
+    ["LimitFragment"] = 100000
+}
 _G.BountyConfig = {
     ["Webhook"] = {
         ["Enabled"] = true,
