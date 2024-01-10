86edc1244cea7721c4ab3f304e83c323a95ae92b
backstuu <59038651+backstuu@users.noreply.github.com>
2024-01-11 01:02:02 +0700

    Update and rename maruhubduy.lua to xroykaitun.lua

--- maruhubduy.lua
+++
@@ -1,3 +0,0 @@
-(getgenv()).key = "MARU2WA7FAN1IQ75RRQ0ONZNC4";
-(getgenv()).id = "1103522787613749289";
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruComkak/main/PCBit.lua"))()
